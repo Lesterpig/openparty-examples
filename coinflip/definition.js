ca05cf2d8272f53324334d635a1566532ee61e06
Lesterpig <contact@lesterpig.com>
2015-03-10 20:11:03 +0100

    Indentation fix

--- coinflip/definition.js
+++ coinflip/definition.js
@@ -1,130 +1,130 @@
 module.exports = function() {
 
-    var playerRole = {
-        actions: {
-            choose: {
-                isAvailable: function(room) {
-                    return room.currentStage === "choose";
-                },
-                type: "select",
-                options: {
-                    choices: ["Head", "Tail"],
-                    submit: "Choose",
-                },
-                execute: function(player, choice) {
-                    player.emit("chatMessage", {message: "You have selected <strong>" + choice + "</strong>" });
-                    player.choice = choice;
-                    if(player.room.players.every(function(p) { return p.player.choice !== null; })) {
-                        player.room.endStage();
-                    }
-                }
-            }
+  var playerRole = {
+    actions: {
+      choose: {
+        isAvailable: function(room) {
+          return room.currentStage === "choose";
         },
-        channels: {}
-    };
-
-
-    return {
-        name: "Coin-Flipping",
-        description: "A simple implementation of Coin-Flipping !",
-        minPlayers: 2,
-        maxPlayers: 5,
-        parameters: [],
-        firstStage: "warmup",
-        stages: {
-            "warmup": {
-                start: function(room, callback) {
-                    callback(null, 2);
-                },
-                end: function(room, callback) {
-                    room.nextStage("choose");
-                }
-            },
-            "choose": {
-                start: function(room, callback) {
-                    room.players.forEach(function(e) {
-                        e.player.choice = null;
-                    });
-                    room.broadcast("chatMessage", {message: "<strong>Do your choice !</strong>"});
-                    callback(null, 20);
-                },
-                end: function(room, callback) {
-                    room.broadcast("clearChat");
-                    room.nextStage("announce");
-                }
-            },
-            "announce": {
-                start: function(room, callback) {
-
-					// Flip the coin
-					var result;
-					if (Math.random() >= 0.5) {
-						result = "Head";
-					} else {
-						result = "Tail";
-					}
-					room.message("Coin result: "+result);
-					
-					// List the winners
-					var winnersList = "";
-					var winnersNumber = 0;
-					
-					room.players.forEach(function(e) {
-                        if (e.player.choice === result) {
-							if (winnersNumber != 0) {
-								winnersList+= ", ";	
-							}
-							
-							winnersList += e.username;
-							winnersNumber++;
-						}
-                    });
-					
-					// Annonce the winners
-                    switch(winnersNumber) {
-                        case 0:
-                            room.message("There is no winner...!");
-                            break;
-                        case 1:
-                            room.message(winnersList + " won!");
-                            break;
-						default:
-							room.message("And the winners are... "+winnersList+"!");
-                    }
-                    callback(null, 5);
-                },
-                end: function(room, callback) {
-                    room.message("<hr />");
-                    room.nextStage("choose");
-                }
-            },
-            "end": {
-                start: function(room, callback) {
-                    callback(null, -1);
-                }
-            }
+        type: "select",
+        options: {
+          choices: ["Head", "Tail"],
+          submit: "Choose",
         },
+        execute: function(player, choice) {
+          player.emit("chatMessage", {message: "You have selected <strong>" + choice + "</strong>" });
+          player.choice = choice;
+          if(player.room.players.every(function(p) { return p.player.choice !== null; })) {
+            player.room.endStage();
+          }
+        }
+      }
+    },
+    channels: {}
+  };
 
-        init: function() {},
 
+  return {
+    name: "Coin-Flipping",
+    description: "A simple implementation of Coin-Flipping !",
+    minPlayers: 2,
+    maxPlayers: 5,
+    parameters: [],
+    firstStage: "warmup",
+    stages: {
+      "warmup": {
+        start: function(room, callback) {
+          callback(null, 2);
+        },
+        end: function(room, callback) {
+          room.nextStage("choose");
+        }
+      },
+      "choose": {
+        start: function(room, callback) {
+          room.players.forEach(function(e) {
+            e.player.choice = null;
+          });
+          room.broadcast("chatMessage", {message: "<strong>Do your choice !</strong>"});
+          callback(null, 20);
+        },
+        end: function(room, callback) {
+          room.broadcast("clearChat");
+          room.nextStage("announce");
+        }
+      },
+      "announce": {
         start: function(room, callback) {
 
-            this.room = room;
+          // Flip the coin
+          var result;
+          if (Math.random() >= 0.5) {
+            result = "Head";
+          } else {
+            result = "Tail";
+          }
+          room.message("Coin result: "+result);
 
-            for(i = 0; i < room.players.length; i++) {
-                room.players[i].player.setRole("player", playerRole);
+          // List the winners
+          var winnersList = "";
+          var winnersNumber = 0;
+
+          room.players.forEach(function(e) {
+            if (e.player.choice === result) {
+              if (winnersNumber != 0) {
+                winnersList+= ", ";
+              }
+
+              winnersList += e.username;
+              winnersNumber++;
             }
-            callback(null);
-            room.broadcast("setGameInfo", "Coin Flipping Simulation");
-            room.broadcast("chatMessage", {message: "<strong>Are you ready ?</strong>" });
-        },
+          });
 
-        processMessage: function(channel, message, player) {
-            return message;
+          // Annonce the winners
+          switch(winnersNumber) {
+            case 0:
+              room.message("There is no winner...!");
+              break;
+            case 1:
+              room.message(winnersList + " won!");
+              break;
+            default:
+              room.message("And the winners are... "+winnersList+"!");
+          }
+          callback(null, 5);
         },
-
-        onDisconnect: function(room, player) {
-            room.nextStage("end");
+        end: function(room, callback) {
+          room.message("<hr />");
+          room.nextStage("choose");
         }
+      },
+      "end": {
+        start: function(room, callback) {
+          callback(null, -1);
+        }
+      }
+    },
+
+    init: function() {},
+
+    start: function(room, callback) {
+
+      this.room = room;
+
+      for(i = 0; i < room.players.length; i++) {
+        room.players[i].player.setRole("player", playerRole);
+      }
+      callback(null);
+      room.broadcast("setGameInfo", "Coin Flipping Simulation");
+      room.broadcast("chatMessage", {message: "<strong>Are you ready ?</strong>" });
+    },
+
+    processMessage: function(channel, message, player) {
+      return message;
+    },
+
+    onDisconnect: function(room, player) {
+      room.nextStage("end");
     }
+  }
 };
--- rps/definition.js
+++ rps/definition.js
@@ -1,127 +1,127 @@
 module.exports = function() {
 
-    var playerRole = {
-        actions: {
-            choose: {
-                isAvailable: function(room) {
-                    return room.currentStage === "choose";
-                },
-                type: "select",
-                options: {
-                    choices: ["Rock", "Paper", "Scissors"],
-                    submit: "Choose",
-                },
-                execute: function(player, choice) {
-                    player.emit("chatMessage", {message: "You have selected <strong>" + choice + "</strong>" });
-                    player.choice = choice;
-                    if(player.room.players.every(function(p) { return p.player.choice !== null; })) {
-                        player.room.endStage();
-                    }
-                }
-            }
+  var playerRole = {
+    actions: {
+      choose: {
+        isAvailable: function(room) {
+          return room.currentStage === "choose";
         },
-        channels: {}
-    };
-
-
-    return {
-        name: "Rock-Paper-Scissors",
-        description: "A simple implementation of Rock-Paper-Scissors against an other player!",
-        minPlayers: 2,
-        maxPlayers: 2,
-        parameters: [],
-        firstStage: "warmup",
-        stages: {
-            "warmup": {
-                start: function(room, callback) {
-                    callback(null, 2);
-                    e.player.score = 0;
-                },
-                end: function(room, callback) {
-                    room.nextStage("choose");
-                }
-            },
-            "choose": {
-                start: function(room, callback) {
-                    room.players.forEach(function(e) {
-                        e.player.choice = null;
-                    });
-                    room.broadcast("chatMessage", {message: "<strong>Do your choice !</strong>"});
-                    callback(null, 20);
-                },
-                end: function(room, callback) {
-                    room.broadcast("clearChat");
-                    room.nextStage("announce");
-                }
-            },
-            "announce": {
-                start: function(room, callback) {
-
-                    var aChoice = room.players[0].player.choice;
-                    var bChoice = room.players[1].player.choice;
-
-                    var score = 1;
-
-                    if(aChoice === "Rock"     && bChoice === "Scissors" ||
-                       aChoice === "Paper"    && bChoice === "Rock"     ||
-                       aChoice === "Scissors" && bChoice === "Paper")
-                        score = -1;
-
-                    if(aChoice === bChoice)
-                        score = 0;
-
-                    room.message(room.players[0].username + " has chosen <strong>" + aChoice + "</strong> and " +
-                                 room.players[1].username + " has chosen <strong>" + bChoice + "</strong>.");
-
-                    switch(score) {
-                        case -1:
-                            room.message(room.players[0].username + " won!");
-                            room.players[0].player.score++;
-                            break;
-                        case 1:
-                            room.message(room.players[1].username + " won!");
-                            room.players[1].player.score++;
-                            break;
-                    }
-
-                    for(var i = 0; i < 2; i++) {
-                        room.playerInfo(room.players[i], room.players[i].username + " : " + room.players[i].player.score);
-                    }
-
-                    callback(null, 5);
-                },
-                end: function(room, callback) {
-                    room.message("<hr />");
-                    room.nextStage("choose");
-                }
-            },
-            "end": {
-                start: function(room, callback) {
-                    callback(null, -1);
-                }
-            }
+        type: "select",
+        options: {
+          choices: ["Rock", "Paper", "Scissors"],
+          submit: "Choose",
         },
+        execute: function(player, choice) {
+          player.emit("chatMessage", {message: "You have selected <strong>" + choice + "</strong>" });
+          player.choice = choice;
+          if(player.room.players.every(function(p) { return p.player.choice !== null; })) {
+            player.room.endStage();
+          }
+        }
+      }
+    },
+    channels: {}
+  };
 
-        init: function() {},
 
+  return {
+    name: "Rock-Paper-Scissors",
+    description: "A simple implementation of Rock-Paper-Scissors against an other player!",
+    minPlayers: 2,
+    maxPlayers: 2,
+    parameters: [],
+    firstStage: "warmup",
+    stages: {
+      "warmup": {
+        start: function(room, callback) {
+          callback(null, 2);
+        },
+        end: function(room, callback) {
+          room.nextStage("choose");
+        }
+      },
+      "choose": {
+        start: function(room, callback) {
+          room.players.forEach(function(e) {
+            e.player.choice = null;
+          });
+          room.broadcast("chatMessage", {message: "<strong>Do your choice !</strong>"});
+          callback(null, 20);
+        },
+        end: function(room, callback) {
+          room.broadcast("clearChat");
+          room.nextStage("announce");
+        }
+      },
+      "announce": {
         start: function(room, callback) {
 
-            this.room = room;
+          var aChoice = room.players[0].player.choice;
+          var bChoice = room.players[1].player.choice;
 
-            for(i = 0; i < room.players.length; i++) {
-                room.players[i].player.setRole("player", playerRole);
-            }
-            callback(null);
-            room.broadcast("setGameInfo", "Rock-Paper-Scissors Simulation");
-            room.broadcast("chatMessage", {message: "<strong>Are you ready ?</strong>" });
+          var score = 1;
+
+          if(aChoice === "Rock"     && bChoice === "Scissors" ||
+             aChoice === "Paper"    && bChoice === "Rock"     ||
+             aChoice === "Scissors" && bChoice === "Paper")
+            score = -1;
+
+          if(aChoice === bChoice)
+            score = 0;
+
+          room.message(room.players[0].username + " has chosen <strong>" + aChoice + "</strong> and " +
+                 room.players[1].username + " has chosen <strong>" + bChoice + "</strong>.");
+
+          switch(score) {
+            case -1:
+              room.message(room.players[0].username + " won!");
+              room.players[0].player.score++;
+              break;
+            case 1:
+              room.message(room.players[1].username + " won!");
+              room.players[1].player.score++;
+              break;
+          }
+
+          for(var i = 0; i < 2; i++) {
+            room.playerInfo(room.players[i], room.players[i].username + " : " + room.players[i].player.score);
+          }
+
+          callback(null, 5);
         },
-
-        processMessage: function(channel, message, player) {
-            return message;
-        },
-
-        onDisconnect: function(room, player) {
-            room.nextStage("end");
+        end: function(room, callback) {
+          room.message("<hr />");
+          room.nextStage("choose");
         }
+      },
+      "end": {
+        start: function(room, callback) {
+          callback(null, -1);
+        }
+      }
+    },
+
+    init: function() {},
+
+    start: function(room, callback) {
+
+      this.room = room;
+
+      for(i = 0; i < room.players.length; i++) {
+        room.players[i].player.setRole("player", playerRole);
+        room.players[i].player.score = 0;
+      }
+      callback(null);
+      room.broadcast("setGameInfo", "Rock-Paper-Scissors Simulation");
+      room.broadcast("chatMessage", {message: "<strong>Are you ready ?</strong>" });
+    },
+
+    processMessage: function(channel, message, player) {
+      return message;
+    },
+
+    onDisconnect: function(room, player) {
+      room.nextStage("end");
     }
+  }
 };
